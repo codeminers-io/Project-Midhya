a8fd7b7caa7f7cf6e56fd9bc51b34e9beb24fb45
Ranjith <ranjithsnair@live.com>
2019-12-01 21:37:17 +0800

    Implemented basic NLP chatbot

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 // From https://github.com/howdyai/botkit#basic-usage
 var Botkit = require('botkit');
+var rasa = require('./middleware')({rasa_uri: 'http://localhost:5000'});
 
 var controller = Botkit.slackbot({
   debug: false,
+  clientId: '',
+  clientSecret: '',
+  clientSigningSecret: '',
+  scopes: ['bot'],
+  json_file_store: __dirname + '/.db/'
 });
+
+// Override receive method in botkit
+controller.middleware.receive.use(rasa.receive);
 
 // connect the bot to a stream of messages
 controller.spawn({
-  token: 'xoxb-854945260992-847029510225-1Bt2EB3n3JFyxZMZzhOkQ0Lr',
+  token: '',
   name: 'midhya'
 }).startRTM()
 
-// give the bot something to listen for.
-controller.hears('hello', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
-
-  bot.reply(message, 'I am midhya how can I help you ?');
-
+// Override hears method in botkit
+controller.changeEars(function (patterns, message) {
+  return rasa.hears(patterns, message);
 });
+
+controller.hears([''],'message_received,direct_message,direct_mention,mention', rasa.hears, function(bot, message) {
+
+  console.log('Intent:', message.intent);
+  console.log('Entities:', message.entities);    
+
+  bot.reply(message,"Intent: " + message.intent.name);
+});
+
