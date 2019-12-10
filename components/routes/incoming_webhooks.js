14d837fc8381dd0fa224a7c1337b1753215660df
ranjithsnair <ranjithsnair@live.com>
2019-12-10 13:43:48 +0800

    Update incoming_webhooks.js

--- components/routes/incoming_webhooks.js
+++ components/routes/incoming_webhooks.js
@@ -39,18 +39,21 @@ module.exports = function (webserver, controller) {
     });
     
     webserver.get('/data/import', (req, res) => {
+        fs.createReadStream('data.csv').pipe(csv())
+            .on('data', (row) => {
+                
+                exec("PGPASSFILE=/workdir/.pgpass psql -h ec2-174-129-255-59.compute-1.amazonaws.com -U uzzgeqgptzfgrx -d d7o70knkceadu8 -c \"INSERT INTO \"nodel_officer_details\" (apex_ministry_dept_state, parent_organisation, org_code, org_name, contact_address1, contact_address2, contact_address3, pincode, pg_officer_designation, organisation_levels) VALUES ('" + row[0].replace('\'', '') + "','" + row[1].replace('\'', '') + "','" + row[2].replace('\'', '') + "','" + row[3].replace('\'', '') + "','" + row[4].replace('\'', '') + "','" + row[5].replace('\'', '') + "','" + row[6].replace('\'', '') + "','" + row[7].replace('\'', '') +"','" + row[8].replace('\'', '') + "'," + row[9].replace('\'', '') + ");\"", function(err, data) {  
+                    console.log(err)
+                    console.log(data.toString());
+                });  
 
-        exec("PGPASSFILE=/workdir/.pgpass psql -h ec2-174-129-255-59.compute-1.amazonaws.com -U uzzgeqgptzfgrx -d d7o70knkceadu8 -c \"INSERT INTO \"nodel_officer_details\" (apex_ministry_dept_state, parent_organisation, org_code, org_name, contact_address1, contact_address2, contact_address3, pincode, pg_officer_designation, organisation_levels) VALUES ('BSES Rajdhani / Yamuna Power Ltd','Department of Power','BSESP','BSES Rajdhani / Yamuna Power Ltd','BSES Bhavan','Nehru Place','New Delhi','110019','Chief Executive Officer',4);\"", function(err, data) {  
-            console.log(err)
-            console.log(data.toString());
-            
-            result = {
-                error = err,
-                data = data.toString()
-            }
-            
-            res.status(200).send(result);
-        });  
+            })
+            .on('end', () => {
+                
+                res.status(200).send("Done");
+
+            });
+        
     });
 
 }
