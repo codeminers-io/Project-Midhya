934b96f73808f32888c4add3baa05d138aa6e0c6
ranjithsnair <ranjithsnair@live.com>
2019-12-10 18:11:42 +0800

    Update incoming_webhooks.js

--- components/routes/incoming_webhooks.js
+++ components/routes/incoming_webhooks.js
@@ -1,6 +1,6 @@
-var exec = require('child_process').execFile;
-const csv = require('csv-parser');
-const fs = require('fs');
+const pg = require("pg");
+const csv = require("csv-parser");
+const fs = require("fs");
 
 module.exports = function (webserver, controller) {
 
@@ -40,45 +40,76 @@ module.exports = function (webserver, controller) {
 
     });
     
-    webserver.get('/data/import', (req, res) => {
-        var i = 1;
-        try {
-        fs.createReadStream('/workdir/dataset/NodalOfficer_Details.csv').pipe(csv())
-            .on('data', (row) => {
-                
-                if(i <= 500)
-                {
-                    
-                    var row1 = row['Apex Ministry/Dept/State'] == undefined ? '' : row['Apex Ministry/Dept/State'].replace('\'', '');
-                    var row2 = row['Parent of Organisation'] == undefined ? '' : row['Parent of Organisation'].replace('\'', '');
-                    var row3 = row['org_code'] == undefined ? '' : row['org_code'].replace('\'', '');
-                    var row4 = row['org_name'] == undefined ? '' : row['org_name'].replace('\'', '');
-                    var row5 = row['contact_address1'] == undefined ? '' : row['contact_address1'].replace('\'', '');
-                    var row6 = row['contact_address2'] == undefined ? '' : row['contact_address2'].replace('\'', '');
-                    var row7 = row['contact_address3'] == undefined ? '' : row['contact_address3'].replace('\'', '');
-                    var row8 = row['pincode'] == undefined ? '' : row['pincode'].replace('\'', '');
-                    var row9 = row['pg_officer_desig'] == undefined ? '' : row['pg_officer_desig'].replace('\'', '');
-                    var row10 = row['Organisation levels'] == undefined ? '' : row['Organisation levels'].replace('\'', '');
-    
-                    exec("PGPASSFILE=/workdir/.pgpass /usr/bin/psql -h ec2-174-129-255-59.compute-1.amazonaws.com -U uzzgeqgptzfgrx -d d7o70knkceadu8 -c \"INSERT INTO \"nodel_officer_details\" (apex_ministry_dept_state, parent_organisation, org_code, org_name, contact_address1, contact_address2, contact_address3, pincode, pg_officer_designation, organisation_levels) VALUES ('" + row1 + "','" + row2 + "','" + row3 + "','" + row4 + "','" + row5 + "','" + row6 + "','" + row7 + "','" + row8 +"','" + row9 + "'," + row10 + ");\"", function(err, data) {  
-                        console.log(err)
-                        console.log(data.toString());
-                    });
+    webserver.get("/data/import", (req, res) => {
+    try {
+      var i = 1;
+      const connectionString =
+        "postgres://uzzgeqgptzfgrx:60831dd60ddaf75f4f19cc2f21bfc3c4b96bb5fd94cfe04f044b69fcf1e236fd@ec2-174-129-255-59.compute-1.amazonaws.com:5432/d7o70knkceadu8";
+      const client = new pg.Client(connectionString);
+      client.connect();
 
-                }
+      fs.createReadStream("/workdir/dataset/NodalOfficer_Details.csv")
+        .pipe(csv())
+        .on("data", row => {
+          if (i <= 500) {
+            var row1 =
+              row["Apex Ministry/Dept/State"] == undefined
+                ? ""
+                : row["Apex Ministry/Dept/State"].replace("'", "");
+            var row2 =
+              row["Parent of Organisation"] == undefined
+                ? ""
+                : row["Parent of Organisation"].replace("'", "");
+            var row3 =
+              row["org_code"] == undefined
+                ? ""
+                : row["org_code"].replace("'", "");
+            var row4 =
+              row["org_name"] == undefined
+                ? ""
+                : row["org_name"].replace("'", "");
+            var row5 =
+              row["contact_address1"] == undefined
+                ? ""
+                : row["contact_address1"].replace("'", "");
+            var row6 =
+              row["contact_address2"] == undefined
+                ? ""
+                : row["contact_address2"].replace("'", "");
+            var row7 =
+              row["contact_address3"] == undefined
+                ? ""
+                : row["contact_address3"].replace("'", "");
+            var row8 =
+              row["pincode"] == undefined
+                ? ""
+                : row["pincode"].replace("'", "");
+            var row9 =
+              row["pg_officer_desig"] == undefined
+                ? ""
+                : row["pg_officer_desig"].replace("'", "");
+            var row10 =
+              row["Organisation levels"] == undefined
+                ? ""
+                : row["Organisation levels"].replace("'", "");
 
-                i++;
-
-            })
-            .on('end', () => {
-                
-                res.status(200).send("Done");
-
+            const query = client.query(
+              "INSERT INTO \"nodel_officer_details\" (apex_ministry_dept_state, parent_organisation, org_code, org_name, contact_address1, contact_address2, contact_address3, pincode, pg_officer_designation, organisation_levels) VALUES ('" + row1 + "','" + row2 + "','" + row3 + "','" + row4 + "','" + row5 + "','" + row6 + "','" + row7 + "','" + row8 +"','" + row9 + "'," + row10 + ");"
+            );
+            query.on("end", () => {
+              console.log("Imported...")
             });
-        }
-        catch (e) {
-            res.status(200).send(e);
-        }
-    });
+          }
+
+          i++;
+        })
+        .on("end", () => {
+          client.end();
+          res.status(200).send("Done");
+        });
+    } catch (e) {
+      res.status(200).send(e);
+    }
+  });
 
 }
